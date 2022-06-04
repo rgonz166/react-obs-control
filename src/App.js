36543d77dc074ace5a4f840cf9b4da464424cc96
Raul Gonzalez <rgonz166@gmail.com>
2022-06-04 12:42:57 -0700

    Connect to websocket

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React, { Component } from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-function App() {
+const client = new W3CWebSocket('ws://localhost:4444');
 
+class App extends Component {
+
+  componentDidMount() {
+    client.onopen = () => {
+      console.log('Websocket Client Connected')
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Hello</h3>
+      </div>
+    )};
   
-
-  return (
-    <div>
-      <h3>Hello</h3>
-    </div>
-  );
 }
 
 export default App;
