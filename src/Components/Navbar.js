5ef5815549b23b888ae604db662c1331df8338e3
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-06-05 00:27:15 -0700

    changed navbar color, fixed code spacing

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,53 +9,52 @@ const Navbar = () => {
 
     return (
         <>
-             <Box shadow='md' w='100%' p={4} bgGradient='linear(to-r, black.000, blue.500, black.300)' color='white'>
-            <HStack>
-                <Flex w="100%">
-                    <Heading pl={20}>OBS Control</Heading>
+            <Box shadow='md' w='100%' p={4} bg="#20293a" color='white'>
+                <HStack>
+                    <Flex w="100%">
+                        <Heading pl={20}>OBS Control</Heading>
 
-                    <Spacer></Spacer>
-                    <Flex display={['none', 'none', 'flex', 'flex']}>
-                    <ButtonGroup gap='.03' variant='ghost' size='sm' mt={2}  _hover={{ color:'black' }}>
-                    
-                        <Link to='/'>
-                            <Button>
-                                Homepage
-                            </Button>
-                        </Link>
-                        <Link to='/ChannelPoints'>
-                            <Button>
-                                Channel Points
-                            </Button>
-                        </Link>
-                        <Link to='/Bits'>
-                            <Button>
-                                Bits
-                            </Button>
-                        </Link>
-                        <Link to='/Subscription'>
-                            <Button>
-                                Subscriptions
-                            </Button>
-                        </Link>
+                        <Spacer></Spacer>
+                        <Flex display={['none', 'none', 'flex', 'flex']}>
+                        <ButtonGroup gap='.03' variant='ghost' size='sm' mt={2}  _hover={{ color:'black' }}>
+                        
+                            <Link to='/'>
+                                <Button>
+                                    Homepage
+                                </Button>
+                            </Link>
+                            <Link to='/ChannelPoints'>
+                                <Button>
+                                    Channel Points
+                                </Button>
+                            </Link>
+                            <Link to='/Bits'>
+                                <Button>
+                                    Bits
+                                </Button>
+                            </Link>
+                            <Link to='/Subscription'>
+                                <Button>
+                                    Subscriptions
+                                </Button>
+                            </Link>
 
-                        <Link to='/Settings'>
-                            <Button>
-                                Settings
-                            </Button>
-                        </Link>
+                            <Link to='/Settings'>
+                                <Button>
+                                    Settings
+                                </Button>
+                            </Link>
 
-                </ButtonGroup>
-                </Flex>
-                <Tooltip hasArrow label="Toggle Light/Dark Mode">
-                    <Box>
-                        <ColorModeSwitcher justifySelf="flex-end" />                    
-                    </Box>
-                </Tooltip>
-
-             </Flex>
-            </HStack>
-        </Box>
+                        </ButtonGroup>
+                    </Flex>
+                        <Tooltip hasArrow label="Toggle Light/Dark Mode">
+                            <Box>
+                                <ColorModeSwitcher justifySelf="flex-end" />                    
+                            </Box>
+                        </Tooltip>
+                    </Flex>
+                </HStack>
+            </Box>
         </>
     )
 }
