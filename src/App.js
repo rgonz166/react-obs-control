8ab8203c59cd6e81e900f4527c9cc1080fdc414b
Raul Gonzalez <rgonz166@gmail.com>
2022-06-15 18:06:32 -0700

    Added: event listeners for twitch

--- src/App.js
+++ src/App.js
@@ -57,14 +57,6 @@ const App = () => {
       })
     }
 
-    ComfyJS.onChat = (user, message, flags, self, extra) => {
-      console.log('user', user);
-      console.log('message', message);
-      console.log('flags', flags);
-      console.log('self', self);
-      console.log('extra', extra);
-    }
-
     ComfyJS.onError = (err) => {
       console.error('err', err)
       toast({
@@ -75,6 +67,70 @@ const App = () => {
         isClosable: true
       })
     }
+
+    ComfyJS.onChat = (user, message, flags, self, extra) => {
+      console.log('user', user);
+      console.log('message', message);
+    }
+
+    ComfyJS.onCheer = (user, message, bits, flags, extra) => {
+      console.log('user', user);
+      console.log('message', message);
+      console.log('bits', bits);
+    }
+
+    ComfyJS.onGiftSubContinue = (user, sender, extra) => {
+      console.log('user', user);
+      console.log('sender', sender);
+      console.log('extra', extra);
+    }
+    
+    ComfyJS.onHosted = (user, viewers, autohost, extra) => {
+      console.log('user', user);
+      // number
+      console.log('viewers', viewers);
+      console.log('autohost?', autohost);
+      console.log('extra', extra);
+    }
+
+    ComfyJS.onRaid = (user, viewers) => {
+      console.log('user', user)
+      console.log('viewers', viewers)
+    }
+
+    ComfyJS.onReward = (user, reward, cost, message, extra) => {
+      console.log('user', user);
+      console.log('reward', reward);
+      console.log('cost', cost)
+      console.log('message', message);
+      console.log('extra', extra);
+    }
+
+    ComfyJS.onResub = (user, message, streakMonths, cumulativeMonths, subTierInfo, extra) => {
+      console.log('user', user);
+      console.log('message', message);
+      console.log('streakMonths', streakMonths);
+      console.log('cumulativeMonths', cumulativeMonths);
+      console.log('subTierInfo', subTierInfo);
+      console.log('extra', extra);
+    }
+
+    ComfyJS.onSub = (user, message, subTierInfo, extra) => {
+      console.log('user', user);
+      console.log('message', message);
+      console.log('subTierInfo', subTierInfo);
+      console.log('extra', extra);
+    }
+    
+    ComfyJS.onSubMysteryGift = (gifterUser, numbOfSubs, senderCount, subTierInfo, extra) => {
+      console.log('gifterUser', gifterUser);
+      console.log('numbOfSubs', numbOfSubs);
+      console.log('senderCount', senderCount);
+      console.log('subTierInfo', subTierInfo);
+      console.log('extra', extra);
+    }
+
+
 
     // ComfyJS.onChat()
 
