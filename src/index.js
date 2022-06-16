661494fed38f5a054b62aa5461c2576e9f0a8a90
Raul Gonzalez <rgonz166@gmail.com>
2022-06-15 21:49:05 -0700

    Add: OBS Context

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import './index.css';
 import App from './App';
+import { ObsProvider } from 'Contexts/ObsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ChakraProvider theme={theme}>
-        <App />
+        <ObsProvider>
+            <App />
+        </ObsProvider>
     </ChakraProvider>
 );
