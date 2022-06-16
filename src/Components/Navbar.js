7c6ece1ec28d2dedc31f5d33549bb397fda1a61d
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-06-15 22:59:05 -0700

    added hamburger menu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ButtonGroup, Button, HStack, Box, Flex, Heading, Spacer, Tooltip } from "@chakra-ui/react"
+import { ButtonGroup, Button, HStack, Box, Flex, Heading, Spacer, Tooltip, IconButton, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
+import { HamburgerIcon } from "@chakra-ui/icons";
 import { ColorModeSwitcher } from "../Components/ColorModeSwitcher"
 
 
 const Navbar = () => {
-
+    
 
     return (
         <>
@@ -13,12 +14,12 @@ const Navbar = () => {
                 <HStack>
                     <Flex w="100%">
                         <Link to='/'>
-                            <Heading pl={20}>OBS Control</Heading>
+                            <Heading pl={21}>OBS Control</Heading>
                         </Link>
 
                         <Spacer></Spacer>
-                        <Flex display={['none', 'none', 'flex', 'flex']}>
-                        <ButtonGroup gap='.03' variant='ghost' size='sm' mt={2}  _hover={{ color:'black' }}>
+                        <Flex display={{ base: "none", md: 'block' }}>
+                        <ButtonGroup gap='.03' variant='ghost' size='sm' mt={2} color='white'>
                         
                             <Link to='/'>
                                 <Button>
@@ -49,6 +50,43 @@ const Navbar = () => {
 
                         </ButtonGroup>
                     </Flex>
+                    <Menu>
+                        <MenuButton 
+                         display={{ base: "block", md: "none" }}
+                         as={IconButton}
+                         aria-label='Options'
+                         icon={<HamburgerIcon />}
+                         variant='outline'
+
+                        />
+                        <MenuList>
+                            <Link to='/'>
+                                <MenuItem >
+                                    <Button w={'100%'}>Home</Button>
+                                </MenuItem>
+                            </Link>
+                            <Link to='/ChannelPoints'>
+                                <MenuItem >
+                                    <Button w={'100%'}>ChannelPoints</Button>
+                                </MenuItem>
+                            </Link>
+                            <Link to='/Bits'>
+                                <MenuItem >
+                                    <Button w={'100%'}>Bits</Button>
+                                </MenuItem>
+                            </Link>
+                            <Link to='/Subscriptions'>
+                                <MenuItem >
+                                    <Button w={'100%'}>Subscriptions</Button>
+                                </MenuItem>
+                            </Link>
+                            <Link to='/Settings'>
+                                <MenuItem >
+                                    <Button w={'100%'}>Settings</Button>
+                                </MenuItem>
+                            </Link>
+                    </MenuList>
+                    </Menu>
                         <Tooltip hasArrow label="Toggle Light/Dark Mode">
                             <Box>
                                 <ColorModeSwitcher justifySelf="flex-end" />                    
