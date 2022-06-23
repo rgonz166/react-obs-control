3d9c9c7461739dc1071f5e6db5f2e0b29ac89b05
Raul Gonzalez <rgonz166@gmail.com>
2022-06-23 00:59:28 -0700

    Remove on events from twitch context

--- src/Contexts/TwitchContext.js
+++ src/Contexts/TwitchContext.js
@@ -32,71 +32,6 @@ export function TwitchProvider ({children}) {
             isClosable: true
           })
         }
-    
-        ComfyJS.onChat = (user, message, flags, self, extra) => {
-          console.log('user', user);
-          console.log('message', message);
-        }
-    
-        ComfyJS.onCheer = (user, message, bits, flags, extra) => {
-          console.log('user', user);
-          console.log('message', message);
-          console.log('bits', bits);
-        }
-    
-        ComfyJS.onGiftSubContinue = (user, sender, extra) => {
-          console.log('user', user);
-          console.log('sender', sender);
-          console.log('extra', extra);
-        }
-        
-        ComfyJS.onHosted = (user, viewers, autohost, extra) => {
-          console.log('user', user);
-          // number
-          console.log('viewers', viewers);
-          console.log('autohost?', autohost);
-          console.log('extra', extra);
-        }
-    
-        ComfyJS.onRaid = (user, viewers) => {
-          console.log('user', user)
-          console.log('viewers', viewers)
-        }
-    
-        ComfyJS.onReward = (user, reward, cost, message, extra) => {
-          console.log('user', user);
-          console.log('reward', reward);
-          console.log('cost', cost)
-          console.log('message', message);
-          console.log('extra', extra);
-        }
-    
-        ComfyJS.onResub = (user, message, streakMonths, cumulativeMonths, subTierInfo, extra) => {
-          console.log('user', user);
-          console.log('message', message);
-          console.log('streakMonths', streakMonths);
-          console.log('cumulativeMonths', cumulativeMonths);
-          console.log('subTierInfo', subTierInfo);
-          console.log('extra', extra);
-        }
-    
-        ComfyJS.onSub = (user, message, subTierInfo, extra) => {
-          console.log('user', user);
-          console.log('message', message);
-          console.log('subTierInfo', subTierInfo);
-          console.log('extra', extra);
-        }
-        
-        ComfyJS.onSubMysteryGift = (gifterUser, numbOfSubs, senderCount, subTierInfo, extra) => {
-          console.log('gifterUser', gifterUser);
-          console.log('numbOfSubs', numbOfSubs);
-          console.log('senderCount', senderCount);
-          console.log('subTierInfo', subTierInfo);
-          console.log('extra', extra);
-        }
-    
-    
-    
         // ComfyJS.onChat()
     
       }, [toast]);
@@ -162,12 +97,13 @@ export function TwitchProvider ({children}) {
             // variables and function that will be used globally
             value={
                 {
-                    twitchConnected, setTwitchConnected,
-                    twitchUsername, setTwitchUsername,
-                    token, setToken,
-                    connectTwitchEvents, disconnectTwitchEvents,
-                    getTwitch, clientId,
-                    getPointRewards
+                  ComfyJS,
+                  twitchConnected, setTwitchConnected,
+                  twitchUsername, setTwitchUsername,
+                  token, setToken,
+                  connectTwitchEvents, disconnectTwitchEvents,
+                  getTwitch, clientId,
+                  getPointRewards
                 }
             }
         >
