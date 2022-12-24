dfac6a5e88348f830093886330ee21824946f544
Raul Gonzalez <rgonz166@gmail.com>
2022-12-24 00:47:45 -0800

    Add Bits handling

--- src/Pages/Bits.js
+++ src/Pages/Bits.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Heading, VStack, Text, HStack } from '@chakra-ui/react';
+import React, { useContext } from 'react';
+import { Heading, VStack, Text, HStack, Center, Button } from '@chakra-ui/react';
 import ObsOptions from 'Components/ObsOptions';
 import InputNumber from 'Components/InputNumber';
 import { useState } from 'react';
+import { ObsContext } from 'Contexts/ObsContext';
+import BitsTable from 'Components/BitsTable';
 
 const Bits = () => {
+
+    const {handleSaveDisabled, addBitsReward} = useContext(ObsContext)
 
     const [minBits, setMinBits] = useState(0);
     const [maxBits, setMaxBits] = useState(0);
@@ -22,11 +26,18 @@ const Bits = () => {
                         </VStack>
                         <VStack>
                             <Text fontSize='2xl'>Max Value</Text>
-                            <InputNumber defaultVal={maxBits} min={0} handleValue={setMaxBits} value={maxBits} />
+                            <InputNumber defaultVal={maxBits} min={minBits} handleValue={setMaxBits} value={maxBits} />
                         </VStack>
                     </HStack>
                     <ObsOptions />
             </VStack>
+            <Center>
+                <HStack padding={'15px 0'}>
+                    <Button disabled={handleSaveDisabled() || (minBits === 0 && maxBits === 0)} onClick={() => addBitsReward(minBits, maxBits)}>Add to List</Button>
+                </HStack>
+            </Center>
+
+            <BitsTable />
         </form>
         </>
     )
